07eb31fd30c6eb4ca354b46bacd54f44cd7d543f
poviea <62376737+poviea@users.noreply.github.com>
2022-08-05 18:18:33 +0900

    Update content.js

--- mini_test/js/content.js
+++ mini_test/js/content.js
@@ -1,15 +1,13 @@
 const btns = document.querySelectorAll(".btns li");
 const boxs = document.querySelectorAll("section article");
 
-for(let i =0; i<btns.length; i++) {
+for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', () => {
+        for (let j = 0; j < btns.length; j++) {
+            boxs[j].classList.remove('on');
+            btns[j].classList.remove('on');
+        }
         boxs[i].classList.add('on');
         btns[i].classList.add('on');
-    });
-};
-for(let i =0; i<btns.length; i++) {
-    btns[i].addEventListener('mouseout', () => {
-        boxs[i].classList.remove('on')
-        btns[i].classList.remove('on')
     });
 };
